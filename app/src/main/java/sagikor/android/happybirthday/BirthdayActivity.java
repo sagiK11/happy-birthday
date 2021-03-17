8fcb25b28e4b75c45984ed089b38d00edb45446c
Sagi Korzack <sagi1193@gmail.com>
2021-03-17 21:45:03 +0200

    Binds views

---
+++ app/src/main/java/sagikor/android/happybirthday/BirthdayActivity.java
@@ -0,0 +1,12 @@
+package sagikor.android.happybirthday;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BirthdayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/sagikor/android/happybirthday/MainActivity.java
+++ app/src/main/java/sagikor/android/happybirthday/MainActivity.java
@@ -3,27 +3,50 @@ package sagikor.android.happybirthday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button bOpenBirthDayScreen;
+    ImageButton bAddImage;
     DatePickerDialog datePickerDialog;
+    TextView tBirthdayDate;
+    EditText tName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linkViews();
+        bindViews();
+        addViewsOnClickListeners();
     }
 
-    private void linkViews() {
-        button = findViewById(R.id.button_open_birthday_screen);
-        //button.setOnClickListener( e -> openBirthdayScreen());
+    private void addViewsOnClickListeners() {
+        tBirthdayDate.setOnClickListener(e -> getBirthday());
+        bAddImage.setOnClickListener( e -> addImage());
+        bOpenBirthDayScreen.setOnClickListener( e -> openBirthdayScreen());
+    }
 
+
+    private void bindViews() {
+        tName = findViewById(R.id.name_input);
+        tBirthdayDate = findViewById(R.id.birthday_date);
+        bAddImage = findViewById(R.id.button_add_image);
+        bOpenBirthDayScreen = findViewById(R.id.button_open_birthday_screen);
     }
 
     private void openBirthdayScreen() {
+        startActivity(new Intent(this,BirthdayActivity.class));
+    }
+
+    private void getBirthday() {
+    }
+
+    private void addImage() {
     }
 }
